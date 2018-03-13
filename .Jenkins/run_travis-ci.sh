efb615faa7012339b11b916a04ff09a16c130168
Mark D Horn <mark.d.horn@intel.com>
2018-03-13 16:12:14 -0700

    Change the Go lang version based on .travis.yml
    The public Travis-CI will select the Go version based
    on the .travis.yml; implementing this in the Docker image.
    
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- .Jenkins/run_travis-ci.sh
+++ .Jenkins/run_travis-ci.sh
@@ -9,6 +9,17 @@
 
 rvm use 2.3.0
 
+# Ensure we are using the correct version of Go Lang
+go_current=$(gimme -l |& grep current | awk '{print $1}')
+eval $(/bin/bash ${WORKSPACE}/.Jenkins/yaml.sh)
+if [ ${go_} != ${go_current} ]
+then
+    gimme ${go_} > ${HOME}/go_ver.sh
+    echo -n "${go_}" > $HOME/.gimme/version
+    . ${HOME}/go_ver.sh
+    rm ${HOME}/go_ver.sh
+fi
+
 # Compile the .travis.yml into a Travis-CI script
 cd ${WORKSPACE}
 /home/travis/.travis/travis-build/bin/travis compile > ${HOME}/travis-ci.sh
