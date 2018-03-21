50c76a3e019b4fed85786b2f5bb71f7289667f50
Leandro Dorileo <leandro.maciel.dorileo@intel.com>
2018-03-21 12:08:34 -0700

    storage: add frontend support stuffs
    Added block device model and state fields to reflect the detected
    device's.
    
    Added methods to convert sizes to "human readable" formatted strings
    and a method to return a standard partitioning set.
    
    Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

--- src/clr-installer/storage/storage.go
+++ src/clr-installer/storage/storage.go
@@ -16,12 +16,14 @@ import (
 // A BlockDevice describes a block device and its partitions
 type BlockDevice struct {
 	Name            string         // device name
+	Model           string         // device model
 	MajorMinor      string         // major:minor device number
 	FsType          string         // filesystem type
 	UUID            string         // filesystem uuid
 	MountPoint      string         // where the device is mounted
 	Size            float64        // size of the device
 	Type            int            // device type
+	State           int            // device state (running, live etc)
 	ReadOnly        bool           // read-only device
 	RemovableDevice bool           // removable device
 	Children        []*BlockDevice // children devices/partitions
@@ -36,6 +38,15 @@ const (
 
 	// BlockDeviceTypeRom identifies a BlockDevice as a rom
 	BlockDeviceTypeRom = 3
+
+	// BlockDeviceStateUnknown identifies a BlockDevice in a unknown state
+	BlockDeviceStateUnknown = 0
+
+	// BlockDeviceStateRunning identifies a BlockDevice as running
+	BlockDeviceStateRunning = 1
+
+	// BlockDeviceStateLive identifies a BlockDevice as live
+	BlockDeviceStateLive = 2
 )
 
 var (
@@ -43,10 +54,111 @@ var (
 	storageExp  = regexp.MustCompile(`([0-9](\.)?[0-9]*)?([m,g,t,k,p])`)
 )
 
+// Validate checks if the minimal requirements for a installation is met
+func (bd *BlockDevice) Validate() error {
+	bootPartition := false
+	rootPartition := false
+
+	for _, ch := range bd.Children {
+		if ch.FsType == "vfat" && ch.MountPoint == "/boot" {
+			bootPartition = true
+		}
+
+		if ch.MountPoint == "/" {
+			rootPartition = true
+		}
+	}
+
+	if !bootPartition {
+		return errors.Errorf("Could not find a suitable EFI partition")
+	}
+
+	if !rootPartition {
+		return errors.Errorf("Could not find a root partition")
+	}
+
+	return nil
+}
+
+// RemoveChild removes a partition from disk block device
+func (bd *BlockDevice) RemoveChild(child *BlockDevice) {
+	nList := []*BlockDevice{}
+
+	for _, curr := range bd.Children {
+		if curr == child {
+			continue
+		}
+
+		nList = append(nList, curr)
+	}
+
+	bd.Children = nList
+}
+
+// AddChild adds a partition to a disk block device
+func (bd *BlockDevice) AddChild(child *BlockDevice) {
+	if bd.Children == nil {
+		bd.Children = []*BlockDevice{}
+	}
+
+	bd.Children = append(bd.Children, child)
+
+	if child.Name == "" {
+		child.Name = fmt.Sprintf("%s%d", bd.Name, len(bd.Children))
+	}
+}
+
+// HumanReadableSize converts the size representation in bytes to the closest
+// human readable format i.e 10M, 1G, 2T etc
+func HumanReadableSize(size float64) (string, error) {
+	sizes := []struct {
+		unit string
+		mask float64
+	}{
+		{"P", 1 << 50},
+		{"T", 1 << 40},
+		{"G", 1 << 30},
+		{"M", 1 << 20},
+		{"K", 1 << 10},
+	}
+
+	for _, curr := range sizes {
+		csize := size / curr.mask
+		if csize < 1 {
+			continue
+		}
+
+		return fmt.Sprintf("%.2f%s", csize, curr.unit), nil
+	}
+
+	return "", fmt.Errorf("Could not format desk/partition size")
+}
+
+// FreeSpace returns the block device available/free space considering the currently
+// configured partition table
+func (bd *BlockDevice) FreeSpace() (float64, error) {
+	if bd.Type != BlockDeviceTypeDisk {
+		return 0, errors.Errorf("FreeSpace() must only be called with a disk block device")
+	}
+
+	var total float64
+	for _, curr := range bd.Children {
+		total = total + curr.Size
+	}
+
+	return bd.Size - total, nil
+}
+
+// HumanReadableSize converts the size representation in bytes to the closest
+// human readable format i.e 10M, 1G, 2T etc
+func (bd *BlockDevice) HumanReadableSize() (string, error) {
+	return HumanReadableSize(bd.Size)
+}
+
 // ListBlockDevices Lists all available/attached block devices
 func ListBlockDevices() ([]*BlockDevice, error) {
 	w := bytes.NewBuffer(nil)
-	err := cmd.Run(w, false, lsblkBinary, "-J", "-b")
+	err := cmd.Run(w, false, lsblkBinary, "-J", "-b", "-O")
 	if err != nil {
 		return nil, fmt.Errorf("%s", w.String())
 	}
@@ -56,38 +168,7 @@ func ListBlockDevices() ([]*BlockDevice, error) {
 		return nil, err
 	}
 
-	w = bytes.NewBuffer(nil)
-	err = cmd.Run(w, false, lsblkBinary, "-J", "-f")
-	if err != nil {
-		return nil, fmt.Errorf("%s", w.String())
-	}
-
-	fs, err := parseBlockDevicesDescriptor(w.Bytes())
-	if err != nil {
-		return nil, err
-	}
-
-	baseMp := map[string]*BlockDevice{}
-	fsMp := map[string]*BlockDevice{}
-
-	mapDescriptors(baseMp, bd)
-	mapDescriptors(fsMp, fs)
-
-	for k, v := range baseMp {
-		v.FsType = fsMp[k].FsType
-	}
-
 	return bd, nil
-}
-
-func mapDescriptors(mp map[string]*BlockDevice, bds []*BlockDevice) {
-	for _, curr := range bds {
-		mp[curr.Name] = curr
-
-		for _, ch := range curr.Children {
-			mapDescriptors(mp, []*BlockDevice{ch})
-		}
-	}
 }
 
 func parseBlockDevicesDescriptor(data []byte) ([]*BlockDevice, error) {
@@ -134,7 +215,9 @@ func getNextBoolToken(dec *json.Decoder, name string) (bool, error) {
 	return false, errors.Errorf("Unknown ro value: %s", str)
 }
 
-func parseVolumeSize(str string) (float64, error) {
+// ParseVolumeSize will parse a string formated (1M, 10G, 2T) size and return its representation
+// in bytes
+func ParseVolumeSize(str string) (float64, error) {
 	str = strings.ToLower(str)
 
 	if !storageExp.MatchString(str) {
@@ -189,6 +272,15 @@ func (bd *BlockDevice) UnmarshalJSON(b []byte) error {
 			}
 
 			bd.Name = name
+		case "model":
+			var model string
+
+			model, err = getNextStrToken(dec, "model")
+			if err != nil {
+				return err
+			}
+
+			bd.Model = model
 		case "maj:min":
 			var majMin string
 
@@ -206,7 +298,7 @@ func (bd *BlockDevice) UnmarshalJSON(b []byte) error {
 				return err
 			}
 
-			bd.Size, err = parseVolumeSize(size)
+			bd.Size, err = ParseVolumeSize(size)
 			if err != nil {
 				return err
 			}
@@ -245,6 +337,23 @@ func (bd *BlockDevice) UnmarshalJSON(b []byte) error {
 			} else {
 				return errors.Errorf("Unknown block device type: %s", tp)
 			}
+		case "state":
+			var state string
+
+			state, err = getNextStrToken(dec, "state")
+			if err != nil {
+				return err
+			}
+
+			if state == "running" {
+				bd.State = BlockDeviceStateRunning
+			} else if state == "live" {
+				bd.State = BlockDeviceStateLive
+			} else if state == "" {
+				bd.Type = BlockDeviceStateUnknown
+			} else {
+				return errors.Errorf("Unrecognized block device state: %s", state)
+			}
 		case "mountpoint":
 			var mpoint string
 
@@ -274,4 +383,41 @@ func (bd *BlockDevice) UnmarshalJSON(b []byte) error {
 	}
 
 	return nil
+}
+
+// SupportedFileSystems exposes the currently supported file system
+func SupportedFileSystems() []string {
+	return []string{"ext4", "vfat", "swap"}
+}
+
+// NewStandardPartitions will return a list of BlockDevice representing a
+// default set of partitions required for an installation
+func NewStandardPartitions(disk *BlockDevice) []*BlockDevice {
+	bootSize := float64(150 * (1 << 20))
+	swapSize := float64(2 * (1 << 30))
+	rootSize := float64(disk.Size - bootSize - swapSize)
+
+	// TODO review this standard partition schema (maybe add a default configuration)
+	return []*BlockDevice{
+		{
+			Name:       fmt.Sprintf("%s1", disk.Name),
+			Size:       bootSize,
+			Type:       BlockDeviceTypePart,
+			FsType:     "vfat",
+			MountPoint: "/boot",
+		},
+		{
+			Name:   fmt.Sprintf("%s2", disk.Name),
+			Size:   swapSize,
+			Type:   BlockDeviceTypePart,
+			FsType: "swap",
+		},
+		{
+			Name:       fmt.Sprintf("%s3", disk.Name),
+			Size:       rootSize,
+			Type:       BlockDeviceTypePart,
+			FsType:     "ext4",
+			MountPoint: "/",
+		},
+	}
 }
